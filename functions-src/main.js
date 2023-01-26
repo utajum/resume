806179a5c149bebf968b6d8bb6eacdd5234e3bb0
Vladimir Tasic <vladimir.tasik@gmail.com>
2023-01-26 16:23:15 +0100

    update

--- functions-src/main.js
+++ functions-src/main.js
@@ -1,5 +1,11 @@
 export async function handler(event, context) {
-  console.log(event, context);
+  console.log(
+    `{\ncontext: ${JSON.stringify(context, null, 2)},\nevent: ${JSON.stringify(
+      event,
+      null,
+      2
+    )}\n}`
+  );
   return {
     statusCode: 200,
     body: JSON.stringify({
