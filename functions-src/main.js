5d620aeec42ae6fb88e248bbd3b31b7bc40f5ae7
Vladimir Tasic <vladimir.tasik@gmail.com>
2023-01-26 16:50:32 +0100

    update

--- functions-src/main.js
+++ functions-src/main.js
@@ -1,26 +1,39 @@
 export async function handler(event, context) {
-  console.log(
-    `{
-      context: ${JSON.stringify(context, null, 2)},
-      event: ${JSON.stringify(event, null, 2)}
-     }`
-  );
-  console.log(event?.headers?.["host"]);
-  console.log(event?.headers?.["sec-ch-ua-platform"]);
-  console.log(event?.headers?.["user-agent"]);
-  console.log(event?.headers?.["x-country"]);
-  console.log(event?.headers?.["x-language"]);
-  console.log(event?.headers?.["x-nf-client-connection-ip"]);
-  console.log(event?.multiValueHeaders?.["X-Nf-Client-Connection-Ip"]);
-  console.log(event?.multiValueHeaders?.["host"]);
-  console.log(event?.multiValueHeaders?.["X-Language"]);
-  console.log(event?.multiValueHeaders?.["X-Country"]);
-  console.log(event?.multiValueHeaders?.["User-Agent"]);
+  // console.log(JSON.stringify({ context, event }));
+
+  const host =
+    event?.headers?.["host"] ||
+    (event?.multiValueHeaders?.["host"] || "").toString();
+
+  const platform = event?.headers?.["sec-ch-ua-platform"] || "";
+
+  const userAgent =
+    event?.headers?.["user-agent"] ||
+    (event?.multiValueHeaders?.["User-Agent"] || "").toString();
+
+  const country =
+    event?.headers?.["x-country"] ||
+    (event?.multiValueHeaders?.["X-Country"] || "").toString();
+
+  const language =
+    event?.headers?.["x-language"] ||
+    (event?.multiValueHeaders?.["X-Language"] || "").toString();
+
+  const clientIP =
+    event?.headers?.["x-nf-client-connection-ip"] ||
+    (event?.multiValueHeaders?.["X-Nf-Client-Connection-Ip"] || "").toString();
+
+  const clientInfo = {
+    host,
+    clientIP,
+    platform,
+    userAgent,
+    country,
+    language,
+  };
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello world ${Math.floor(Math.random() * 10)}`,
-    }),
+    body: JSON.stringify(clientInfo),
   };
 }
