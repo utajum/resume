7bb06907d7769e9b2f72cff7c8279a2c3d7a3947
Vladimir Tasic <vladimir.tasik@gmail.com>
2023-05-01 15:52:42 +0200

    update

--- functions-src/info.js
+++ functions-src/info.js
@@ -88,7 +88,12 @@ export async function handler(event, context) {
   return {
     statusCode: 200,
     headers: {
+      'access-control-allow-methods': 'POST,OPTIONS,GET',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Max-Age': '2592000',
+      'Access-Control-Allow-Credentials': 'true',
     },
     body: JSON.stringify({ ...other, TOTAL_SITE_VISITS }),
   };
